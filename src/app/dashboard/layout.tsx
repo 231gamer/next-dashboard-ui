b645c6f1b55fa20a714ac77e282b19d6fb70eea3
Foday Malike Kabah <fodaykabah1997@gmail.com>
2024-10-17 06:05:42 +0000

    Navbar comp

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import Image from "next/image"
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
-      return <div>dashboard{children}</div>;
+      return (<div className="h-screen flex">
+        {/*Left*/}
+        <div className="w-[14%]  md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+          <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" alt="Logo Image" width={32} height={32}/>
+          <span className="hidden lg:block">SoftnetAfrica</span>
+          </Link>
+          <Menu/>
+        </div>
+        {/*Right*/}
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar />
+          {children}
+        </div>
+        </div>);
   }
