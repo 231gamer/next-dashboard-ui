a7d256f819c11f020b014cc79488495719637553
Foday Malike Kabah <fodaykabah1997@gmail.com>
2024-10-20 09:15:50 +0000

    Admin Page completed, Calendar, events and announcements components done.

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcement from "@/components/Announcements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalender from "@/components/EventCalendar"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
@@ -32,7 +34,10 @@ const AdminPage = () => {
             </div>
             </div>
             {/* Right  */}
-            <div className="w-full lg:w-1/3">r</div>
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+                <EventCalender />
+                <Announcement />
+            </div>
         </div>
     )
 }
